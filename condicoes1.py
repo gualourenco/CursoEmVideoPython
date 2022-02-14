ac282c1dff77f1b5d8ef2e55c99ce63448c844a8
Gustavo <gualourenco@gmail.com>
2022-02-14 01:19:45 -0300

    atualização em alguns códigos

--- condicoes1.py
+++ condicoes1.py
@@ -1,11 +1,28 @@
+# Exemplos de condição
 
-tempo = int(input("quantos anos tem seu carro? "))
+# tempo = int(input("quantos anos tem seu carro? "))
+# if tempo <= 3:
+#     print('Carro novo')
+# else:
+#     print('Carro velho')
+# print('--FIM--')
+#
+# print('Carro novo' if tempo <= 3 else 'Carro velho')
+# print('--FIM--')
+#
+# nome = str(input('Qual o seu nome? ')).title()
+# if nome == 'Gustavo':
+#     print('Que nome lindo você tem!')
+# else:
+#     print('Seu nome é um nome')
+# print(f'Bom dia {nome}')
 
-if tempo <= 3:
-    print('Carro novo')
+
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print(f'A sua media foi {m:.1f}')
+if m >= 6.0:
+    print('Sua média foi boa! PARABENS!')
 else:
-    print('Carro velho')
-print('--FIM--')
-
-print('Carro novo' if tempo <= 3 else 'Carro velho')
-print('--FIM--')
+    print('Sua média foi ruim! ESTUDE MAIS!')
