20dce3389e6c41ca4c3fc050c0a21e7e057d54e2
Gustavo <gualourenco@gmail.com>
2022-02-17 23:20:16 -0300

    atualização em alguns códigos

--- ex052.py
+++ ex052.py
@@ -1,23 +1,21 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo, retornando de 0 até o mesmo.
 num = int(input('Digite um número para sabermos se é primo:\n'))
 
-for c in range(0, num):
-    if num % 2 == 0:
-        print(f'{c}')
-    elif num % 3 == 0:
-        print(f'{c}')
+for c in range(1, num+1):
+    if num % c == 0:
+        print(f'\033[33m{c}\033[m', end=' ')
     else:
-        print(f'{c}')
+        print(f'\033[31m{c}\033[m', end=' ')
 
 if num % 2 == 0:
     if num == 2:
-        print(f'O numero {num} é primo')
+        print(f'\nO numero {num} é primo')
     else:
-        print(f'O numero {num} não é primo')
+        print(f'\nO numero {num} não é primo')
 elif num % 3 == 0:
     if num == 3:
-        print(f'O numero {num} é primo')
+        print(f'\nO numero {num} é primo')
     else:
-        print(f'O numero {num} não é primo')
+        print(f'\nO numero {num} não é primo')
 else:
-    print(f'O numero {num} é primo')
+    print(f'\nO numero {num} é primo')
